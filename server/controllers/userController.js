86ce5476f5be31d9b864543879fe35a2e542fde7
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 20:51:36 +0200

    server:  middleware for ADMIN roles

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,11 +3,11 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
 
-const generateJwt = (id, email, role)=> {
+const generateJwt = (id, email, role) => {
     return jwt.sign(
         {id, email, role},
         process.env.SECRET_KEY,
-        {expiresIn:'24h'}
+        {expiresIn: '24h'}
     )
 }
 
@@ -31,7 +31,7 @@ class UserController {
 
     async login(req, res, next) {
         const {email, password} = req.body
-        const user = await User.findOne({where:{email}})
+        const user = await User.findOne({where: {email}})
         if (!user) {
             return next(ApiError.badRequest("There is no user with such email"))
         }
@@ -39,12 +39,13 @@ class UserController {
         if (!comparePassword) {
             return next(ApiError.badRequest('Wrong password or email'))
         }
-        const token = generateJwt(user.id,user.email, user.role)
+        const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
 
     async check(req, res, next) {
-
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
