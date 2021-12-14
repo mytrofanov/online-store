5aff3803bb4f546eee75a12218a02aa912cc78cd
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 11:23:27 +0200

    server:  images would be saved in static folder

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,10 +1,22 @@
 const uuid = require('uuid')
+const path = require('path')
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError')
 
 class DeviceController {
-    async create(req, res) {
-        const {name, price, brandId, typeId, info} = req.body
-        const {img} = req.files
-        let fileName = uuid.v4() + '.jpeg'
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + '.jpeg'
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))  //removes files to /static
+
+            const device = await Device.create({name, price, brandId, typeId, img:fileName})
+            return res.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+
     }
 
     async getAll(req, res) {
