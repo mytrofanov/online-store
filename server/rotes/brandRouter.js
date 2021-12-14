0fe3babebdcce53819c21a4566032b57b54ab52d
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 10:34:18 +0200

    server: router & controllers

--- server/rotes/brandRouter.js
+++ server/rotes/brandRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
+const brandController = require('../controllers/brandController')
 
-router.post('/', )
-router.get('/', )
-router.delete('/', )
+router.post('/', brandController.create )
+router.get('/', brandController.getAll )
+
+
 
 
 module.exports = router
