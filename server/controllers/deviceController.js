3b1505581db48d5b9da2cd7281860d4a7377d063
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 10:54:05 +0200

    server: npm i express-fileupload

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,11 +1,14 @@
 class DeviceController {
-    async create (req, res) {
+    async create(req, res) {
+        const {name, price, brandId, typeId, info} = req.body
+        const {img} = req.files
+    }
+
+    async getAll(req, res) {
 
     }
-    async getAll (req, res) {
 
-    }
-    async getOne (req, res) {
+    async getOne(req, res) {
 
     }
 }
