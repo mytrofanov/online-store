4881b521353bda76005959d2f91e1866e9448ac3
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 17:15:15 +0200

    server:  deviceController.js pagination and limits

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -20,19 +20,23 @@ class DeviceController {
     }
 
     async getAll(req, res) {
-        const {brandId, typeId} = req.query
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+
         let devices;
         if (!brandId && !typeId) {
-            devices = await Device.findAll()
+            devices = await Device.findAndCountAll({limit,offset})
         }
         if (brandId && !typeId) {
-            devices = await Device.findAll({where:{brandId}})
+            devices = await Device.findAndCountAll({where:{brandId}, limit, offset})
         }
         if (!brandId && typeId) {
-            devices = await Device.findAll({where:{typeId}})
+            devices = await Device.findAndCountAll({where:{typeId}, limit, offset})
         }
         if (brandId && typeId) {
-            devices = await Device.findAll({where:{brandId, typeId}})
+            devices = await Device.findAndCountAll({where:{brandId, typeId}, limit, offset})
         }
         return res.json(devices)
 
