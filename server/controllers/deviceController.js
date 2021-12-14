b17646814379a2bed1b815c0883b4203968909d2
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 17:29:27 +0200

    server:  deviceController.js -  get one device

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,17 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class DeviceController {
     async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + '.jpeg'
             img.mv(path.resolve(__dirname, '..', 'static', fileName))  //removes files to /static
-
             const device = await Device.create({name, price, brandId, typeId, img: fileName})
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i =>
+                DeviceInfo.create({
+                    title: i.title,
+                    description: i.description,
+                    deviceId: device.id
+                })
+                )
+            }
+
             return res.json(device)
         } catch (e) {
             next(ApiError.badRequest(e.message))
@@ -43,7 +53,13 @@ class DeviceController {
     }
 
     async getOne(req, res) {
-
+        const {id} = req.params
+        const device = await Device.findOne (
+            {where: {id},
+            include:[{model: DeviceInfo, as: 'info'}]
+            }
+        )
+        return res.json(device)
     }
 }
 
