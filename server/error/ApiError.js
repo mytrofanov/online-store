65065061f744caea7233c942d97286800657d6a2
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-14 10:20:46 +0200

    server: API error - no ID

--- server/error/ApiError.js
+++ server/error/ApiError.js
@@ -15,3 +15,4 @@ class ApiError extends Error {
         return new ApiError(403, message)
     }
 }
+module.exports = ApiError
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./rotes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -13,6 +14,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
+
+app.use(errorHandler) // must be on the last place (last MiddleWare)
 
 const start = async () => {
   try {
