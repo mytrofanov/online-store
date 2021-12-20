19fc84e7806dfd987bdcc7d22c5f0f8ce4894a16
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-20 10:48:08 +0200

    client part - creating context for props  index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import UserStore from "./store/UserStore";
 
+const Context = createContext(null)
 
 ReactDOM.render(
+    <Context.Provider value={{user: new UserStore()
+    }
+    }>
+        <App/>
+    </Context.Provider>,
 
-    <App />,
-
-  document.getElementById('root')
-);
+document.getElementById('root')
+)
+;
