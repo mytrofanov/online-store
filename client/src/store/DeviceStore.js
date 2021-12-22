ecb1afd73183141bb32fbf88e133589fb23df64f
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-22 17:40:04 +0200

    client part: made getter and setter for selected types of goods in DeviceStore.js

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -8,15 +8,38 @@ export default class DeviceStore {
             {id: 3, name: 'Телевизоры'},
             {id: 4, name: 'Ноутбуки'},
         ]
-        this._brands = [{id:1, name:'Samsung'},
-            {id:2, name:'Apple'}]
+        this._brands = [{id: 1, name: 'Samsung'},
+            {id: 2, name: 'Apple'}]
         this._device = [
-            {id:1, name:'Веб-камера A4Tech PK-930HA USB Black', rating:5, img:'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'},
-            {id:2, name:'Веб-камера A4Tech PK-930HA USB Black', rating:5, img:'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'},
-            {id:3, name:'Веб-камера A4Tech PK-930HA USB Black', rating:5, img:'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'},
-            {id:4, name:'Веб-камера A4Tech PK-930HA USB Black', rating:5, img:'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'}]
+            {
+                id: 1,
+                name: 'Веб-камера A4Tech PK-930HA USB Black',
+                rating: 5,
+                img: 'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'
+            },
+            {
+                id: 2,
+                name: 'Веб-камера A4Tech PK-930HA USB Black',
+                rating: 5,
+                img: 'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'
+            },
+            {
+                id: 3,
+                name: 'Веб-камера A4Tech PK-930HA USB Black',
+                rating: 5,
+                img: 'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'
+            },
+            {
+                id: 4,
+                name: 'Веб-камера A4Tech PK-930HA USB Black',
+                rating: 5,
+                img: 'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'
+            }
+        ]
+        this._selectedType = {}
         makeAutoObservable(this)
     }
+
 
     setTypes(types) {
         this._types = types
@@ -25,19 +48,28 @@ export default class DeviceStore {
     setBrands(brands) {
         this._brands = brands
     }
+
     setDevices(devices) {
         this._devices = devices
+    }
+
+    setSelectedType(type) {
+        this._selectedType = type
     }
 
     get Types() {
         return this._types
     }
 
-    get brands () {
+    get brands() {
         return this._brands
     }
-    get devices () {
+
+    get devices() {
         return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
     }
 
 
