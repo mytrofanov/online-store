02e070d92fbae6c9775f71d0609c47a5a300d077
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-22 17:46:53 +0200

    client part: now we are able to select types of goods if TypeBar.js

--- client/src/components/TypeBar.js
+++ client/src/components/TypeBar.js
@@ -3,14 +3,20 @@ import {observer} from "mobx-react-lite";
 import {Context} from "../index";
 import {ListGroup} from "react-bootstrap";
 
-const TypeBar = observer( () => {
+const TypeBar = observer(() => {
     const {device} = useContext(Context)
     return (
         <ListGroup>
             {device._types.map(type =>
-            <ListGroup.Item key={type.id}>
-                {type.name}
-            </ListGroup.Item>
+                <ListGroup.Item key={type.id}
+                                style={{cursor: 'pointer'}}
+                                active={type.id === device._selectedType.id}
+                                onClick={() => {
+                                    device.setSelectedType(type)
+                                }}
+                >
+                    {type.name}
+                </ListGroup.Item>
             )
             }
         </ListGroup>
