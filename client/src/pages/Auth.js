5ee408d64c3a32f6f33224f3daf3aaeddd1faf08
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-22 16:33:17 +0200

    client part: auth and registration page done using const isLogin

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import {Button, Card, Container, Form, Row} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
-import {REGISTRATION_ROUTE} from "../utils/consts";
+import {Button, Card,  Container, Form, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(isLogin)
     return (
         <Container className="d-flex justify-content-center align-items-center"
                    style={{height: window.innerHeight - 54}}>
             <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">Авторизация</h2>
+                <h2 className="m-auto">{isLogin? 'Авторизация': 'Регистрация'}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control className="mt-3" placeholder="Введите email"/>
                     <Form.Control className="mt-3" placeholder="Введите пароль"/>
 
                     <Row className="d-flex justify-content-between p-lg-3">
-                        <div>
+                        {isLogin ? <div>
                             Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
                         </div>
-                        <Button className="mt-3" variant={"outline-success"}>Войти</Button>
+                        :<div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+
+                        }
+                        <Button  className="mt-3" variant={"outline-success"}>{
+                            isLogin ? 'Войти' : 'Зарегистрироваться'}</Button>
                     </Row>
 
                 </Form>
