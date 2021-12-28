4d595affea5818a54a5d57b1261f563aa8a05947
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-28 11:46:28 +0200

    client part:  NavBar - made rotes with useNavigate for Login & Admin

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
-import {SHOP_ROUTE} from "../utils/consts";
-import {NavLink} from "react-router-dom";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {NavLink, useNavigate} from "react-router-dom";
 import {observer} from "mobx-react-lite";
 
-const NavBar = observer( () => {
-    const {user} = useContext(Context)
 
+const NavBar = observer(() => {
+    const {user} = useContext(Context)
+    const navigate = useNavigate()
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -15,12 +16,21 @@ const NavBar = observer( () => {
                 <NavLink style={{color: "white", textDecoration: "none"}} to={SHOP_ROUTE}> Nice Device </NavLink>
                 {user._isAuth ?
                     <Nav className="ml-auto" style={{color: "white"}}>
-                        <Button variant="secondary"  className="me-lg-2">Админ панель</Button>
-                        <Button variant="secondary" onClick={()=>{user.setIsAuth(false)}}>Выйти</Button>
+                        <Button variant="secondary"
+                                className="me-lg-2"
+                                onClick={() => {
+                                    navigate(ADMIN_ROUTE)
+                                }}
+                        >Админ панель</Button>
+                        <Button variant="secondary" onClick={() => {
+                            navigate(LOGIN_ROUTE)
+                        }}>Выйти</Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: "white"}}>
-                    <Button variant="secondary" onClick={()=>{user.setIsAuth(true)}}>Авторизация</Button>
+                        <Button variant="secondary" onClick={() => {
+                            user.setIsAuth(true)
+                        }}>Авторизация</Button>
 
                     </Nav>
                 }
