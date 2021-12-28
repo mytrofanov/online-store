9996051f9402d476775e389f72e8834a2fb9d1e3
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-28 09:37:48 +0200

    client part: useNavigate is used instead of useHistory. Made navigation to devices

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import {Card, Col, Image} from "react-bootstrap";
+import {useNavigate} from "react-router-dom"
+import {DEVICE_ROUTE} from "../utils/consts";
 
 const DeviceItem = ({device}) => {
+    const navigate = useNavigate();
+
+
     return (
-        <Col md={3}>
-            <Card style={{width:150, cursor:'pointer'}} border={'light'}>
+        <Col md={3} className={"mt-3"} onClick={()=>{navigate(DEVICE_ROUTE + '/' + device.id)}}>
+            <Card style={{width: 150, cursor: 'pointer'}} border={'light'}>
                 <Image src={device.img} width={150} height={150}/>
-               <div> {device.name}</div>
-               <div> rating: {device.rating}</div>
+                <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
+                <div>Samsung</div>
+                <div> Rating {device.rating} <span style={{}}>☆</span></div>
+                </div>
+                <div> {device.name} </div>
             </Card>
 
         </Col>
