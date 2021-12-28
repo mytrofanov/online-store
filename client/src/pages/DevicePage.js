604cfda6fb8e18dab4e5cd3e84b94487e8b242aa
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-28 09:44:12 +0200

    client part: creating devicePage

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Col, Container, Image} from "react-bootstrap";
 
 const DevicePage = () => {
+    const device =  {
+            id: 1,
+            name: 'Веб-камера A4Tech PK-930HA USB Black',
+            rating: 5,
+            img: 'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'
+        }
+
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className="mt-3">
+            <Col md={4}>
+                <Image width={300} height={300} src={device.img}/>
+            </Col>
+            <Col md={4}>
+
+            </Col>
+            <Col md={4}>
+
+            </Col>
+        </Container>
     );
 };
 
