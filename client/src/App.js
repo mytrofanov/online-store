beceab4e12e08bd0292b4cc119b042d4075963f8
mytrofanov <mytrofanovmaxym@gmail.com>
2021-12-30 19:43:03 +0200

    client part: spinner for first loading & auth in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
+import {observer} from "mobx-react-lite";
+import {useContext, useEffect, useState} from "react";
+import {Context} from "./index";
+import {check} from "./http/userAPI";
+import {Spinner} from "react-bootstrap";
 
-function App() {
+const App = observer( () => {
+    const {user} = useContext(Context)
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        check().then(data=>{
+            user.setUser(true)
+            user.setIsAuth(true)
+        }).finally(()=>setLoading(false))
+    },[])
+
+
+    if (loading) {
+        return (
+            <div style={{height:'100vh', paddingLeft:'50%'}} >
+            <Spinner animation={"border"} role={"status"} style={{marginTop:'50%'} } />
+            </div>
+        )
+    }
+
     return (
         <BrowserRouter>
             <NavBar/>
             <AppRouter/>
         </BrowserRouter>
     );
-}
+})
 
 export default App;
