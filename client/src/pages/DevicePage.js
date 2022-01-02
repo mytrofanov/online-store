a9568152e1d6cfce1ed35c23ba690f832b326155
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-02 17:14:02 +0200

    client part:  getting one device func

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import star from '../img/Five-pointed_star.svg'
+import {useParams} from "react-router";
+import {fetchOneDevice} from "../http/deviceAPI";
 
 const DevicePage = () => {
-    const device = {
-        id: 1,
-        name: 'Веб-камера A4Tech PK-930HA USB Black',
-        rating: 5,
-        price: 1200,
-        img: 'https://content1.rozetka.com.ua/goods/images/big/218996956.jpg'
-    }
-    const description = [
-        {id: 1, title: 'Разрешение видео', description: 'FullHD (1920x1080)'},
-        {id: 2, title: 'Сенсор', description: 'CMOS'},
-        {id: 3, title: 'Встроенный микрофон', description: 'С микрофоном'},
-        {id: 4, title: 'Фокусировка', description: 'Автофокус'},
-        {id: 5, title: 'Страна-производитель', description: 'Китай'},
-    ]
+    const [device, setDevices] = useState({info:[]})
+    const {id} = useParams()
 
+    useEffect(()=>{
+    fetchOneDevice(id).then(data=>setDevices(data))
+    },[])
     return (
         <Container className="mt-3">
             <Row>
                 <Col md={4}>
-                    <Image width={300} height={300} src={device.img}/>
+                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img}/>
                 </Col>
                 <Col md={4}>
                     <Row className="d-flex flex-column align-items-center">
@@ -49,7 +42,7 @@ const DevicePage = () => {
             </Row>
             <Row className="d-flex flex-column m-3">
                 <h2>Характеристики</h2>
-                {description.map((info, index) =>
+                {device.info.map((info, index) =>
                     <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
                         {info.title} : {info.description}
                     </Row>
