38c5ead698d9ab13b3dfd71dd1232bd73d6977fd
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-07 14:34:23 +0200

    server part: made one more id for errors in requests of User

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,6 +6,7 @@ module.exports = function (req, res, next) {
     }
     try {
         const token = req.headers.authorization.split(' ')[1] //we need second word in string: (Bearer token)
+
         if (!token){
             return res.status(401).json({message:"user not authorized, MiddleWare"})
         }
@@ -13,6 +14,7 @@ module.exports = function (req, res, next) {
         if (!req.user) {
             return res.status(401).json({message:"user not authorized. No User in request"})
         }
+
         req.user = decoded
         next()
     } catch (e) {
