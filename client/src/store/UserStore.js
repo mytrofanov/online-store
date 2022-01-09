b981629647f48336a1e2f3597b3efe1c5d3c96ae
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-09 16:14:30 +0200

    client-part: created variable isAdmin in UserStore.js

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -3,12 +3,16 @@ import {makeAutoObservable} from "mobx";
 export default class UserStore {
     constructor() {
         this._isAuth = false
+        this._isAdmin = false
         this._triedToLogin = false  //needed not to make check request of user status
         this._user = {}
         makeAutoObservable(this)
     }
     setIsAuth(bool) {
         this._isAuth = bool
+    }
+    setIsAdmin(bool) {
+        this._isAdmin = bool
     }
     setTriedToLogin(bool) {
         this._triedToLogin = bool
@@ -18,6 +22,9 @@ export default class UserStore {
     }
     get isAuth(){
         return this._isAuth
+    }
+    get isAdmin(){
+        return this._isAdmin
     }
     get triedToLogin(){
         return this._triedToLogin
