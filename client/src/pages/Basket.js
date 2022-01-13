10e702c1151227a7e434eb4219a3d47ded3354d2
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-13 19:24:44 +0200

    client-part: made basket page

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -1,27 +1,68 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Button, Modal} from "react-bootstrap";
+import {Button, ListGroup, Modal} from "react-bootstrap";
 import {Context} from "../index";
 import {getBasket} from "../http/basketAPI";
+import {fetchOneDevice} from "../http/deviceAPI";
 
 
-const Basket = ({onHide,show}) => {
-    const{basket} = useContext(Context)
-    const{user} = useContext(Context)
+const Basket = ({onHide, show}) => {
+    const {basket} = useContext(Context)
+    const [priceList, setPriceList] = useState([])
+    let totalSum = 0
     const pay = () => {
-
         basket.setBasketVisible(false)
     }
     let basketId = basket.basketId
+    function makePriceList(array) {
+        let b = []
+        let c = array
+        let idSet = new Set()
 
-    useEffect(()=>{
-            if(basketId && basket.basketVisible) {
+        c.forEach(i=>{
+            if(!idSet.has(i.id)){
+                b.push(i)
+            }
+            if(idSet.has(i.id)){
+                b.forEach(j=>{
+                    if(j.id === i.id) {j.quality +=1}
+                })
+            }
+            idSet.add(i.id)
+        })
+        b.forEach(q=>{
+            q.summ = q.quality * q.price
+        })
+        setPriceList(b)
+    }
+
+    useEffect(() => {
+        if (basketId && basket.basketVisible) {
             console.log('basketId:' + basketId)
-            getBasket(basketId).then(data=>{
-                console.log(data)
-                // basket.setBasketDevices(data)
+            let tempArray = []
+            getBasket(basketId).then(data => {
+                data.map(async (i) => {
+                    await fetchOneDevice(i.deviceId).then(devices => {
+                            devices !==undefined && tempArray.push(devices)
+                        }
+                    ).then(data=>
+                        tempArray.forEach(i => i.quality = 1)
+                    ).then(data=>
+                        makePriceList(tempArray)
+                    )
+                })
             })
         }
-    },[basket.basketVisible])
+    }, [basket.basketVisible])
+
+    function totalSumToPay() {
+        let sum
+        priceList.forEach(item=>
+            sum += item.summ
+        )
+        totalSum = sum
+    }
+    totalSumToPay()
+
 
     return (
         <Modal
@@ -37,14 +78,30 @@ const Basket = ({onHide,show}) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                Здесь будут товары
+                <ListGroup>
+                    {priceList.map((device, index)=>
+                        <ListGroup.Item
+                            key={device.id}
+                            variant={index % 2 === 0 ? 'success' : 'light'}>
+
+                            Название: {device.name}
+                            Цена: {device.price}
+                            Количество: {device.quality}
+                            Сумма: {device.summ}
+                        </ListGroup.Item>
+
+                    )}
+                    <ListGroup.Item variant="info">Итого: {totalSum}</ListGroup.Item>
+                </ListGroup>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="outline-danger" onClick={()=>{
+                <Button variant="outline-danger" onClick={() => {
                     basket.setAskForBasket(false)
                     onHide()
                 }}>Закрыть</Button>
-                <Button variant="outline-success" onClick={() => {pay()}}>Заказать</Button>
+                <Button variant="outline-success" onClick={() => {
+                    pay()
+                }}>Заказать</Button>
             </Modal.Footer>
         </Modal>
     );
