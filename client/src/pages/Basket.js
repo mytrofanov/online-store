dbbaa4e7e4716eecad7575a9b9c59fa5765dc642
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-14 17:07:47 +0200

    client-part: minus function in basket

--- client/src/pages/Basket.js
+++ client/src/pages/Basket.js
@@ -6,176 +6,170 @@ import {fetchOneDevice} from "../http/deviceAPI";
 import {observer} from "mobx-react-lite";
 
 const Basket = observer(({onHide, show}) => {
-    const {basket} = useContext(Context)
-    const [priceList, setPriceList] = useState([])
-    const [totalSum, setTotalSum] = useState(0)
-    const [basketInfo, setBasketInfo] = useState([])
-    const [basketChange, setBasketChange] = useState(false)
+            const {basket} = useContext(Context)
+            const [priceList, setPriceList] = useState([])
+            const [totalSum, setTotalSum] = useState(0)
+            const [basketInfo, setBasketInfo] = useState([])
+            const [basketChange, setBasketChange] = useState(false)
 
-    let basketId = basket.basketId
+            let basketId = basket.basketId
 
-    const pay = () => {
-        basket.setBasketVisible(false)
-    }
-    const plus = (deviceId) => {
-        const formData = new FormData()
-        formData.append('deviceId', deviceId)
-        formData.append('basketId', basketId)
-        putInBasket(formData).then(data=>{
-            console.log(data)
-            setBasketChange(basketChange=>!basketChange)
+            const pay = () => {
+                basket.setBasketVisible(false)
             }
-        )
-    }
-    const delAllFromBasket = () => {
-        try {
-            clearBasket({basketId}).then(data => {
-                    // console.log(data)
-                    basket.setBasketEmpty(true)
-                }
-            )
-        } catch (e) {
-            console.log(e)
-        }
-
-    }
-    const delOneFromBasket = (deviceId) => {
-        const formData = new FormData()
-        formData.append('deviceId', deviceId)
-        formData.append('basketId', basketId)
-        try {
-            delOneFromBasket(formData)
-        } catch (e) {
-            console.log(e)
-        }
-    }
-    const minus = (deviceId) => {
-        let single = 0
-        basketInfo.forEach(i => {
-                if (i.deviceId === deviceId) {
-                    return single = i.id
-                }
-            }
-        )
-        try {
-            if (single !== 0) {
-                delSingleFromBasket(single).then(data=>{
-                    console.log(data)
-                    setBasketChange(basketChange=>!basketChange)
-                })
-            }
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    function makePriceList(array) {
-        let b = []
-        let c = array
-        let idSet = new Set()
-        let sum = 0
-        c.forEach(i => {
-            if (!idSet.has(i.id)) {
-                b.push(i)
-            }
-            if (idSet.has(i.id)) {
-                b.forEach(j => {
-                    if (j.id === i.id) {
-                        j.quality += 1
+            const plus = (deviceId) => {
+                const formData = new FormData()
+                formData.append('deviceId', deviceId)
+                formData.append('basketId', basketId)
+                putInBasket(formData).then(data => {
+                        console.log(data)
+                        setBasketChange(basketChange => !basketChange)
                     }
-                })
+                )
             }
-            idSet.add(i.id)
-        })
-        b.forEach(q => {
-            q.summ = q.quality * q.price
-        })
-
-        b.forEach(item =>
-            sum += item.summ
-        )
-        setTotalSum(sum)
-        setPriceList(b)
-    }
-
-    useEffect(() => {
-        if (basketId && basket.basketVisible) {
-            let tempArray = []
-            getBasket(basketId).then(data => {
-                setBasketInfo(data)
-                data.map(async (i) => {
-                    await fetchOneDevice(i.deviceId).then(devices => {
-                            devices !== undefined && tempArray.push(devices)
-                        }
-                    ).then(data =>
-                        tempArray.forEach(i => i.quality = 1)
-                    ).then(data => {
-                            makePriceList(tempArray)
-                            tempArray.length>0 && basket.setBasketEmpty(false)
+            const delAllFromBasket = () => {
+                try {
+                    clearBasket({basketId}).then(data => {
+                            // console.log(data)
+                            basket.setBasketEmpty(true)
                         }
                     )
-                })
-            })
-        }
-    }, [basket.basketVisible, basketChange, basket.askForBasket])
-
-
-    return (
-        <Modal
-            show={show}
-            onHide={onHide}
-            size="lg"
-
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Корзина
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {basket.basketEmpty === true ? <div>Корзина пуста</div> : <ListGroup>
-                    {priceList.map((device, index) =>
-                        <div key={device.id + device.name} style={{display: 'flex', flexdirection: 'raw'}}>
-                            <ListGroup.Item
-                                key={device.id}
-                                variant={index % 2 === 0 ? 'success' : 'light'}>
-
-                                Название: {device.name}
-                                Цена: {device.price}
-                            </ListGroup.Item>
-                            <Button variant="outline-danger" onClick={() => {
-                                minus(device.id)
-                            }}>−</Button>
-                            Количество: {device.quality}
-                            <Button variant="outline-danger" onClick={() => {
-                                plus(device.id)
-                            }}>+</Button>
-                            Сумма: {device.summ}
-                            <Button variant="outline-danger" onClick={() => {
-
-                            }}>Удалить из корзины</Button>
-                        </div>
-                    )}
-                    <ListGroup.Item variant="info">Итого: {totalSum}</ListGroup.Item>
-                </ListGroup>
+                } catch (e) {
+                    console.log(e)
                 }
 
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="outline-dark" onClick={() => {
-                    delAllFromBasket()
-                }}>Очистить корзину</Button>
-                <Button variant="outline-danger" onClick={() => {
-                    basket.setAskForBasket(false)
-                    onHide()
-                }}>Закрыть</Button>
-                <Button variant="outline-success" onClick={() => {
-                    pay()
-                }}>Заказать</Button>
-            </Modal.Footer>
-        </Modal>
-    );
-});
+            }
+            const delOneFromBasket = (deviceId) => {
+                const formData = new FormData()
+                formData.append('deviceId', deviceId)
+                formData.append('basketId', basketId)
+                try {
+                    delOneFromBasket(formData)
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+            const minus = (deviceId) => {
+                for (let i = 0; i < basketInfo.length; i++) {
+                    if (basketInfo[i].deviceId === deviceId) {
+                        return delSingleFromBasket({id:basketInfo[i].id}).then(data=>
+                            setBasketChange(basketChange=>!basketChange)
+                        )
+                    }
+                }
+
+            }
+
+
+            function makePriceList(array) {
+                let b = []
+                let c = array
+                let idSet = new Set()
+                let sum = 0
+                c.forEach(i => {
+                    if (!idSet.has(i.id)) {
+                        b.push(i)
+                    }
+                    if (idSet.has(i.id)) {
+                        b.forEach(j => {
+                            if (j.id === i.id) {
+                                j.quality += 1
+                            }
+                        })
+                    }
+                    idSet.add(i.id)
+                })
+                b.forEach(q => {
+                    q.summ = q.quality * q.price
+                })
+
+                b.forEach(item =>
+                    sum += item.summ
+                )
+                setTotalSum(sum)
+                setPriceList(b)
+            }
+
+            useEffect(() => {
+                if (basketId && basket.basketVisible) {
+                    let tempArray = []
+                    getBasket(basketId).then(data => {
+                        setBasketInfo(data)
+                        data.map(async (i) => {
+                            await fetchOneDevice(i.deviceId).then(devices => {
+                                    devices !== undefined && tempArray.push(devices)
+                                }
+                            ).then(data =>
+                                tempArray.forEach(i => i.quality = 1)
+                            ).then(data => {
+                                    makePriceList(tempArray)
+                                    tempArray.length > 0 && basket.setBasketEmpty(false)
+                                }
+                            )
+                        })
+                    })
+                }
+            }, [basket.basketVisible, basketChange, basket.askForBasket])
+
+
+            return (
+                <Modal
+                    show={show}
+                    onHide={onHide}
+                    size="lg"
+
+                    centered
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            Корзина
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {basket.basketEmpty === true ? <div>Корзина пуста</div> : <ListGroup>
+                            {priceList.map((device, index) =>
+                                <div key={device.id + device.name} style={{display: 'flex', flexdirection: 'raw'}}>
+                                    <ListGroup.Item
+                                        key={device.id}
+                                        variant={index % 2 === 0 ? 'success' : 'light'}>
+
+                                        Название: {device.name}
+                                        Цена: {device.price}
+                                    </ListGroup.Item>
+                                    <Button variant="outline-danger" onClick={() => {
+                                        minus(device.id)
+                                    }}>−</Button>
+                                    Количество: {device.quality}
+                                    <Button variant="outline-danger" onClick={() => {
+                                        plus(device.id)
+                                    }}>+</Button>
+                                    Сумма: {device.summ}
+                                    <Button variant="outline-danger" onClick={() => {
+
+                                    }}>Удалить из корзины</Button>
+                                </div>
+                            )}
+                            <ListGroup.Item variant="info">Итого: {totalSum}</ListGroup.Item>
+                        </ListGroup>
+                        }
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="outline-dark" onClick={() => {
+                            delAllFromBasket()
+                        }}>Очистить корзину</Button>
+                        <Button variant="outline-danger" onClick={() => {
+                            basket.setAskForBasket(false)
+                            onHide()
+                        }}>Закрыть</Button>
+                        <Button variant="outline-success" onClick={() => {
+                            pay()
+                        }}>Заказать</Button>
+                    </Modal.Footer>
+                </Modal>
+            );
+        }
+    )
+;
 
 export default Basket;
