d99afedc85ce72315c806b777c528c8e8a3ac325
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-15 18:25:03 +0200

    client-part: device-item style

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card, Col, Image} from "react-bootstrap";
 import {useNavigate} from "react-router"
 import {DEVICE_ROUTE} from "../utils/consts";
+import s from './style/DeviceItem.module.css'
 
 const DeviceItem = ({device, brands}) => {
     const navigate = useNavigate();
@@ -10,8 +11,8 @@ const DeviceItem = ({device, brands}) => {
         <Col md={3} className={"mt-3"} onClick={() => {
             navigate(DEVICE_ROUTE + '/' + device.id)
         }}>
-            <Card style={{width: 150, cursor: 'pointer'}} border={'light'}>
-                <Image src={process.env.REACT_APP_API_URL + device.img} width={150} height={150}/>
+            <Card className={s.card} border={'light'}>
+                <Image src={process.env.REACT_APP_API_URL + device.img} className={s.image}/>
                 <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
 
                     {  brands.map(brand =>
