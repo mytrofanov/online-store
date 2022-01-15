5571e9ad6c48e2c465235f5a2a522311519aba20
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-15 14:41:42 +0200

    client-part: DevicePage styles

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,6 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
-import star from '../img/Five-pointed_star.svg'
 import noImage from '../img/no-image.png'
 import {useNavigate, useParams} from "react-router";
 import {deleteDevice, fetchOneDevice} from "../http/deviceAPI";
@@ -11,7 +10,8 @@ import EditDeviceInfo from "../components/modals/editDeviceInfo";
 import AddDeviceInfo from "../components/modals/addDeviceInfo";
 import {observer} from "mobx-react-lite";
 import {putInBasket} from "../http/basketAPI";
-
+import s from './style/DevicePage.module.css'
+import InfoModal from "../components/modals/infoModal";
 
 const DevicePage = observer(() => {
             const [oneDevice, setOneDevices] = useState({info: []})
@@ -49,63 +49,68 @@ const DevicePage = observer(() => {
                     basket.setBasketEmpty(false)
                 })
             }
-            const imageOfDevice = (oneDevice.img === undefined) ? noImage : process.env.REACT_APP_API_URL + oneDevice.img
+            const imageOfDevice = (oneDevice.img === undefined) ?
+                noImage : process.env.REACT_APP_API_URL + oneDevice.img
 
 
             return (
                 <Container className="mt-3">
                     <Row>
                         <Col md={4}>
-                            <Image width={300} height={300} src={imageOfDevice}/>
+                            <Image className={s.image} src={imageOfDevice}/>
                         </Col>
                         <Col md={4}>
                             <Row className="d-flex flex-column align-items-center">
                                 <h2>
                                     {oneDevice.name}
                                 </h2>
-                                <div className="d-flex align-items-center justify-content-center"
-                                     style={{
-                                         background: `url(${star}) no-repeat center center`,
-                                         width: 150, height: 150, backgroundSize: 'cover',
-                                         fontSize: 55
-                                     }}>
+                                <div className={s.rating}>
                                     {oneDevice.rating}
                                 </div>
                             </Row>
                         </Col>
                         <Col md={4}>
-                            <Card className="d-flex flex-column align-items-center justify-content-around"
-                                  style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}>
-                                <h3>{oneDevice.price} грн.</h3>
-                                <Button variant={"outline-dark"}
-                                        onClick={() => {
-                                            if (basket.basketId) {
-                                                putDeviceInBasket(deviceId, basket.basketId)
+                            <Card
+                                className="d-flex flex-column align-items-center justify-content-around">
+                                <div className={s.addToBasketCard}>
+                                    <h3>{oneDevice.price} грн.</h3>
+                                    <Button variant={"outline-dark"}
+                                            onClick={() => {
+                                                if (basket.basketId) {
+                                                    putDeviceInBasket(deviceId, basket.basketId)
+                                                }
+                                                !basket.basketId &&  info.setInfoShopVisible(true)
+                                                info.setInfoShop('Пользователь не авторизован')
+                                            }}
+                                    >Добавить в корзину</Button>
 
-                                            }
-                                            !basket.basketId && console.log('Пользователь не авторизован')
-                                        }}
-                                >Добавить в корзину</Button>
+                                </div>
 
-                                {user.isAdmin && <div>
-                                    <Button variant={"outline-danger"}
+                                {user.isAdmin && <div className={s.adminMenu}>
+                                    <h5>Меню Администратора:</h5>
+
+                                    <Button variant={"outline-danger"} size="sm"
+                                            className={'mt-2'}
                                             onClick={() => {
                                                 delDevice(id)
                                             }}
                                     >Удалить товар</Button>
-                                    <Button variant={"outline-success"}
+                                    <Button variant={"outline-success"} size="sm"
+                                            className={'mt-2'}
                                             onClick={() => {
                                                 setEditVisible(true)
                                             }}
                                     >Редактировать параметры товара</Button>
 
-                                    <Button variant={"outline-danger"}
+                                    <Button variant={"outline-danger"} size="sm"
+                                            className={'mt-2'}
                                             onClick={() => {
                                                 setEditInfoVisible(true)
                                             }}
                                     >Изменить характеристики товара</Button>
 
-                                    <Button variant={"outline-success"}
+                                    <Button variant={"outline-success"} size="sm"
+                                            className={'mt-2'}
                                             onClick={() => {
                                                 setInfoAddVisible(true)
                                             }}
@@ -141,6 +146,12 @@ const DevicePage = observer(() => {
                                    onHide={() => {
                                        setInfoAddVisible(false)
                                    }}/>
+                    <InfoModal show={info.infoShopVisible}
+                               infoMessage={info.infoShop}
+                               onHide={() => {
+                                   info.setInfoShopVisible(false)
+                                   info.setInfoShop('')
+                               }}/> {/*shows the result of any action*/}
 
                 </Container>
             );
