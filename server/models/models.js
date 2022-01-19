390af77250d4fc07d09bb0d0a00acca7a40809dd
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-19 08:47:55 +0200

    server-part: changed data-type in info table to TEXT. Changed the name of dataBase

--- server/models/models.js
+++ server/models/models.js
@@ -41,7 +41,7 @@ const Rating = sequelize.define('rating', {
 const DeviceInfo = sequelize.define('device_info', {
     id:{type:DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING,  allowNull: false},
-    description: {type: DataTypes.STRING, allowNull: false}
+    description: {type: DataTypes.TEXT, allowNull: false}
 })
 
 // connections describing:
