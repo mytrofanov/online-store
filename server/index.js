c4fd82b5770e20e05410dc67183c093e1a8349bb
mytrofanov <mytrofanovmaxym@gmail.com>
2022-01-23 21:53:01 +0200

    server-part: deploy

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 
-app.use(cors())
+app.use(cors({origin:"http://nicedevice.top"}))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static'))) //for getting img from static folder
 app.use(fileUpload({}))
