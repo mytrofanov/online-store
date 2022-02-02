3568de9e284a87b79becbc0560df91b9a75911c8
mytrofanov <mytrofanovmaxym@gmail.com>
2022-02-02 18:14:28 +0200

    client part: rating with num of reviews

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -22,15 +22,28 @@ const DeviceItem = ({device, brands, reviews}) => {
         let sumOfRates = 0
         let count = 0
         let tRating = 0
+        let star = '☆'
+        let Text = ({number}) => {
+            if (number === 1 ) {return (<span>отзыв </span>)}
+            if (number > 1 && number <5 ) {return (<span>отзыва </span>)}
+            if (number > 5 ) {return (<span>отзывов </span>)}
+        }
         reviews.reviews.forEach(item => {
             if (item.deviceId === deviceId) {
                 sumOfRates += item.rate
                 count +=1
     }})
         count > 0 ? tRating = sumOfRates / count : tRating = 0
+        let ratingStar = star.repeat(Math.round(tRating))
         return (
             <span id={reviews.reviews.id}>
-                {tRating > 0 ? <span>Rating {tRating.toFixed(1)}☆</span>  : <span></span>}
+                {tRating > 0 ?
+                    <span>
+                    <b>{ratingStar} </b>
+                    <span>{count}</span>
+                        <Text number = {count}/>
+                     </span>
+                    : <span></span>}
             </span>
         )
 
@@ -42,16 +55,14 @@ const DeviceItem = ({device, brands, reviews}) => {
             gotoDevice(device.id)
         }}>
             <Card className={s.card} border={'light'}>
-                <div className={s.rating}><RatingCount deviceId = {device.id}/> </div>
                 <Image src={process.env.REACT_APP_API_URL   +  device.img} className={s.image}/>
                 <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
-
                     {  brands.map(brand =>
                         brand.id === device.brandId && brand.name )
                     }
-
-
                 </div>
+                <div className={s.rating}>
+                    <RatingCount deviceId = {device.id}/> </div>
                 <div> {device.name} </div>
             </Card>
 
